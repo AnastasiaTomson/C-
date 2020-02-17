d21d77bebcbccfc433e742236d1d26676b21d76f
Nastya <tech@apex-crimea.com>
2020-02-17 21:36:36 +0300

    Исправлена логика в соответствии с ТЗ

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,15 +7,9 @@ namespace ConsoleApplication1
     {
         public static void Main(string[] args)
         {
-            string catName, message = " Введите имя кошки \n" +
-                                      "------------------------------------------- \n" +
-                                      " Требования к имени: \n" +
-                                      "1. Не должно содержать цифр; \n" +
-                                      "2. Минимальная длинна имени 3, а максимальная 15; \n" +
-                                      "3. Русские символы; \n" +
-                                      "4. Не пустое значение. \n" +
-                                      "------------------------------------------- \n";
+            string catName, message;
             string menu = "------------------------------------------- \n" +
+                          " Назвать кошку - нажмите 0 \n" +
                           " Накормить кошку - нажмите 1 \n" +
                           " Наказать кошку - нажмите 2 \n" +
                           " Здоровье кошки - нажмите 3 \n" +
@@ -24,50 +18,54 @@ namespace ConsoleApplication1
                           "------------------------------------------- \n";
             bool isNum, isRu;
             short key;
-            do
-            {
-                Console.WriteLine(message);
-                catName = Console.ReadLine();
-                isNum = false;
-                for (int i = 0; i < catName.Length; i++)
-                {
-                    try
-                    {
-                        int.Parse(catName[i].ToString());
-                        isNum = true;
-                        i = catName.Length;
-                    }
-                    catch (Exception){ continue; }
-                }
-
-                bool any = false;
-                foreach (var wordByte in catName)
-                {
-                    if (wordByte > 127)
-                    {
-                        any = true;
-                        break;
-                    }
-                }
-                if (any)
-                {
-                    isRu = false;
-                }
-                else
-                {
-                    isRu = true;
-                }
-                message = "Неверно введено имя, попробуйте еще раз!";
-            } while (catName == "" || catName.Length < 3 || catName.Length > 15 || isNum || isRu);
-            Cat cat = new Cat(catName);
-            Boolean c = true;
-            Console.WriteLine(menu);            
-            while (c)
+            Cat cat = new Cat();
+            Console.WriteLine(menu);
+            while (true)
             {
                 Int16.TryParse(Console.ReadLine(), out key);
                 Console.WriteLine("------------------------------------------");
                 switch (key)
                 {
+                    case 0:
+                        message = " Введите имя кошки \n" +
+                                  "------------------------------------------- \n" +
+                                  " Требования к имени: \n" +
+                                  "1. Не должно содержать цифр; \n" +
+                                  "2. Минимальная длинна имени 3, а максимальная 15; \n" +
+                                  "3. Русские символы; \n" +
+                                  "4. Не пустое значение. \n" +
+                                  "------------------------------------------- \n";
+                        isRu = true;
+                        do
+                        {
+                            Console.WriteLine(message);
+                            catName = Console.ReadLine();
+                            isNum = false;
+                            for (int i = 0; i < catName.Length; i++)
+                            {
+                                try
+                                {
+                                    int.Parse(catName[i].ToString());
+                                    isNum = true;
+                                    i = catName.Length;
+                                }
+                                catch (Exception)
+                                {
+                                    if (catName[i] > 127)
+                                    {
+                                        isRu = false;
+                                    }
+                                    else
+                                    {
+                                        isRu = true;
+                                    }
+                                    continue;
+                                }
+                            }
+                            message = "Неверно введено имя, попробуйте еще раз!";
+                        } while (catName == "" || catName.Length < 3 || catName.Length > 15 || isNum || isRu);
+                        cat.Name = catName;
+                        break;
                     case 1:
                         cat.FeedTheCat();
                         break;
@@ -81,7 +79,7 @@ namespace ConsoleApplication1
                         cat.GetInforamtionCat();
                         break;
                     case 5:
-                        Console.Write(" Пока, я буду скучать! " );
+                        Console.Write(" Пока, я буду скучать! ");
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("<3");
                         Console.ResetColor();
@@ -93,29 +91,36 @@ namespace ConsoleApplication1
                                           menu);
                         break;
                 }
+
                 Console.ResetColor();
                 Console.WriteLine("------------------------------------------");
             }
-             
-
         }
     }
 
     class Cat
-    {
-        public readonly string Name;
-        public int Health = 10;
-
-        public Cat(string name)
+    { 
+        private string name;
+        public string Name
         {
-            Name = name;
+            get { return name; }
+            set
+            {
+                if (name != null)
+                {
+                    Console.WriteLine("Нельзя переименовать кошку! Её имя " + name);
+                }
+                name = value;
+            }
         }
 
+        public int Health = 10;
+        
         public void PunishCat()
         {
             if (Health == 0)
             {
-                Console.Write("Кошку нельзя наказывать, ее здоровье равно ");
+                Console.Write(" Кошку нельзя наказывать, ее здоровье равно ");
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("0");
                 Console.ResetColor();
@@ -137,17 +142,17 @@ namespace ConsoleApplication1
         {
             if (Health == 10)
             {
-                Console.Write("Кошка сыта");
+                Console.Write(" Кошка сыта");
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("  (^ ω ^)");                
             }else
             {
                 Health += 1;
-                Console.Write("Кошка накормлена! Спасибо ");
+                Console.Write(" Кошка накормлена! Спасибо ");
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("<3");
                 Console.ResetColor();
-                Console.Write("Здоровье ");
+                Console.Write(" Здоровье ");
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("+1");
             }
@@ -167,12 +172,13 @@ namespace ConsoleApplication1
             {
                 Console.ForegroundColor = ConsoleColor.Red;
             }
-            Console.WriteLine("Здоровье кошки: " + Health);
+            Console.WriteLine(" Здоровье кошки: " + Health);
         }
 
         public void GetInforamtionCat()
         {
-            Console.WriteLine("Имя кошки: " + Name);
+            Console.WriteLine(" Имя кошки: " + Name);
+
             if (Health <= 10 && Health > 6)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -185,7 +191,7 @@ namespace ConsoleApplication1
             {
                 Console.ForegroundColor = ConsoleColor.Red;
             }
-            Console.WriteLine("Здоровье кошки: " + Health);
+            Console.WriteLine(" Здоровье кошки: " + Health);
         }
     }
 }
